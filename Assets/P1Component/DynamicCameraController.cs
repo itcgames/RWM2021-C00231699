64362a9f8d332d53313037d26ea2a8617ad1bfd3
Robin Meyler <C00231699@itcarlow.ie>
2020-11-16 00:04:04 +0000

    Feature 1 wokring

--- Assets/P1Component/DynamicCameraController.cs
+++ Assets/P1Component/DynamicCameraController.cs
@@ -8,19 +8,20 @@ public class DynamicCameraController : MonoBehaviour
     public Vector2 PermanentOffset = new Vector2(-1,0);
     public float MaxSpeed = 1;
     public float CameraDelay = 0;
+    public bool AccelBasedOnDistance = false;
     public float CameraAcceleration = 1;
 
     private Vector3 lastPlayerPosition;
     private bool CaughtPlayer = true;
     private bool runOnce = false;
     private float timerCounter = 0;
-    private Vector2 catchUpVelocity;
+    private float catchUpVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         Vector3 currentPlayerPosition = Player.transform.position;
-        currentPlayerPosition = currentPlayerPosition - new Vector3(PermanentOffset.x, PermanentOffset.y, 0);
+        //currentPlayerPosition = currentPlayerPosition - new Vector3(PermanentOffset.x, PermanentOffset.y, 0);
         Vector3 newCamPos;
         newCamPos.x = currentPlayerPosition.x;
         newCamPos.y = currentPlayerPosition.y;
@@ -42,47 +43,52 @@ public class DynamicCameraController : MonoBehaviour
     {
         Vector3 currentCameraPosition = transform.position;
         Vector3 currentPlayerPosition = Player.transform.position;
-        Vector3 differenceVector = currentPlayerPosition - lastPlayerPosition;
-        Vector3 newCamPos = new Vector3(0,0,0);
+        Vector3 differenceVector = currentPlayerPosition - currentCameraPosition;
+        Vector3 newCamPos = currentCameraPosition;
 
         Vector2 newDiff = new Vector2(differenceVector.x, differenceVector.y);
-        if (newDiff.magnitude > 1 && runOnce == false)
+        if(CameraDelay <= 0)
+        {
+            newCamPos.x = currentPlayerPosition.x;
+            newCamPos.y = currentPlayerPosition.y;
+        }
+        else if (newDiff.magnitude < 0.1f)
+        {
+            newCamPos.x = currentPlayerPosition.x;
+            newCamPos.y = currentPlayerPosition.y;
+            runOnce = false;
+            CaughtPlayer = true;
+        }
+        else if (newDiff.magnitude > 0.1f && runOnce == false)
         {
             runOnce = true;
             timerCounter = CameraDelay;
             CaughtPlayer = false;
-            catchUpVelocity = new Vector2(0.001f, 0.001f);
+            catchUpVelocity = 0;
         }
-
-        if(true == CaughtPlayer)
-        {
-            runOnce = false;
-            catchUpVelocity = new Vector2(0, 0);
-            //currentPlayerPosition = currentPlayerPosition - new Vector3(PermanentOffset.x, PermanentOffset.y, 0);
-            //newCamPos.x = currentPlayerPosition.x;
-            //newCamPos.y = currentPlayerPosition.y;
-            newCamPos.z = transform.position.z;
-        }
-
-        else if(false == CaughtPlayer)
+       
+     
+        if(false == CaughtPlayer)
         {
             timerCounter -= 0.01f;
             if(timerCounter <= 0.0f)
             {
                 newDiff.Normalize();
 
-                // Up to a max speed- get magnitude and compare it
-                if ((catchUpVelocity * CameraAcceleration).magnitude < MaxSpeed)
+                if ((catchUpVelocity + CameraAcceleration) <= MaxSpeed)
                 {
-                    catchUpVelocity = catchUpVelocity * CameraAcceleration;  // have a rolling velocity,  its set to 0, then increases based on the user added Acceleration
+                    catchUpVelocity += CameraAcceleration; 
                 }
-                newDiff = newDiff + catchUpVelocity;
-                newCamPos.x = currentCameraPosition.x + differenceVector.x;
-                newCamPos.y = currentPlayerPosition.y + differenceVector.y;
-                newCamPos.z = transform.position.z;
+                newDiff = newDiff * catchUpVelocity;
+
+                newCamPos.x = currentCameraPosition.x + newDiff.x/1000;
+                newCamPos.y = currentCameraPosition.y + newDiff.y/1000;
             }
         }
 
+
+
+        newCamPos.z = transform.position.z;
         transform.position = newCamPos;
         lastPlayerPosition = Player.transform.position;
     }
